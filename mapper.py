4071b225f7d2fcc33d24f1e0ed4cb34278bd0bed
Jennifer Christopher <jenniferchristopherr@gmail.com.com>
2025-07-21 14:27:24 +0200

    Exercise 4: Filter only Computers, Cameras, and Video Games categories

--- mapper.py
+++ mapper.py
@@ -34,11 +34,17 @@ for line in sys.stdin:
     # the result is a tuple with 6 elements
     data = line.strip().split("\t")
 
+    if len(data) != 6:
+       raise ValueError("Expected 6 elements, got {}".format(len(data)))
+
     # store the 6 elements of the tuple in seperate variables
     date, time, item, category, sales, payment = data
 
+    if category not in ["Computers", "Cameras", "Video Games"]:
+	continue
+
     # Write the key-value combination to standard output (stdout)
-    # Key is the category, value is the sales     
+    # Ky is the category, value is the sales     
     # With a tab (\t) between key and value
     # New line \n means new record
     sys.stdout.write("{0}\t{1}\n".format(category, sales))
